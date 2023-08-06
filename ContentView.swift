ab5280b327e702690b57efb4f594e5a921ef48a7
insub <vb15@kakao.com>
2023-08-06 12:28:48 +0900

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  TEST
+//
+//  Created by 김인섭 on 2023/07/17.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct ContentView: View {
+    
+    typealias Input = ContentViewInputProcotol
+    typealias Output = ContentViewOutput
+    
+    let input: Input
+    @StateObject var output: Output
+    
+    var body: some View {
+        Button {
+            input.didTapButton()
+        } label: {
+            Text("\(output.count)")
+        }
+    }
+}
+
+extension ContentView {
+    
+    static func build() -> ContentView {
+        let output = ContentViewOutput()
+        let input = ContentViewInput(output: output)
+        return ContentView(input: input, output: output)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView.build()
+    }
+}
---
+++ ContentViewInOut.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewModel.swift
+//  TEST
+//
+//  Created by 김인섭 on 2023/08/04.
+//
+
+import Foundation
+import Combine
+
+protocol ContentViewInputProcotol {
+    
+    var output: ContentViewOutput? { get }
+    func didTapButton()
+}
+
+class ContentViewOutput: ObservableObject {
+    @Published var count: Int = 0
+}
+
+class ContentViewInput: ContentViewInputProcotol {
+    
+    private(set) weak var output: ContentViewOutput?
+    
+    init(output: ContentViewOutput?) {
+        self.output = output
+    }
+    
+    func didTapButton() {
+        output?.count += 1
+    }
+}
